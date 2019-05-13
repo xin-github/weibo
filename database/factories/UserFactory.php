7ebd13e9ded60a94e795460c32358e250d5598a8
vagrant <vagrant@homestead>
2019-05-13 04:38:13 +0000

    用户激活

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'activated' => true,
         'remember_token' => str_random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
